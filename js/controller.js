8652910f30927d821f8dd503a2a30162c834a96c
mwoo526 <mwoo526@naver.com>
2018-03-29 01:52:40 +0900

    [zele-server] controller.js - myCtrl 함수에 변수 초기화

--- js/controller.js
+++ js/controller.js
@@ -1,16 +1,30 @@
 (function(angular){
     'use strict';
     
-    function myCtrl(WeatherService,NewsService,$scope,$interval){
+    function myCtrl(AnnyangService,WeatherService,NewsService,$scope,$interval){
         /* $scope
         * view와 controller의 매개체 역할
         * controller을 통해 scope에 model과 function을 정의해두면 view가 그것을 사용한다.
         * function는 return 값으로 / 변수는 값으로 정의할수 있다.*/
 
         let _this= this;
+        var command = COMMANDS.ko;  //index.html 에 command.js 추가
+        var DEFAULT_COMMAND_TEXT = command.default;
+        $scope.listening = false;
+        $scope.debug = false;
+        $scope.complement = command.hi;
+        $scope.focus = "default";
+        $scope.user = {};
+        $scope.interimResult = DEFAULT_COMMAND_TEXT;
 
+        // Reset the command text
+        var restCommand = function(){
+            $scope.interimResult = DEFAULT_COMMAND_TEXT;
+        }
 
         _this.init=function(){
+
+            restCommand();
 
             let refreshMirrorData =function() {
                 //GeolocationService.init().then(function(){
